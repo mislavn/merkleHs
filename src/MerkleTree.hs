2173af89348b43f2ff36d42c14272194145db294
Mislav Novakovic <mislav.novakovic@gmail.com>
2018-09-16 22:00:53 +0200

    add printMerkleTree function
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

--- src/MerkleTree.hs
+++ src/MerkleTree.hs
@@ -1,4 +1,6 @@
-module MerkleTree ( MerkleTree()
+module MerkleTree (
+    printMerkleTree,
+    MerkleTree(),
     ) where
 
 data MerkleTree a = EmptyMerkleTree
@@ -6,3 +8,13 @@ data MerkleTree a = EmptyMerkleTree
                   deriving (Eq, Ord, Show, Read)
 
 
+printMerkleTree EmptyMerkleTree               = "root node"
+printMerkleTree (MerkleTreeNode a left right) = unlines (printMerkleTree_helper (MerkleTreeNode a left right))
+
+printMerkleTree_helper EmptyMerkleTree               = []
+printMerkleTree_helper (MerkleTreeNode a left right) = show a : printMerkleTree_subtree left right
+        where
+            printMerkleTree_subtree left right =
+                ((showTree "+- " "|  ") (printMerkleTree_helper right))
+                    ++ (showTree "`- " "   ") (printMerkleTree_helper left)
+            showTree first rest = zipWith (++) (first : repeat rest)
