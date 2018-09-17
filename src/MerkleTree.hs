a7d46e2389369be4476468aec6f8129a96677f38
Mislav Novakovic <mislav.novakovic@gmail.com>
2018-09-17 22:21:17 +0200

    add comments
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

--- src/MerkleTree.hs
+++ src/MerkleTree.hs
@@ -3,11 +3,13 @@ module MerkleTree (
     MerkleTree(),
     ) where
 
+-- binary tree representing a merkle tree
 data MerkleTree a = EmptyMerkleTree
                   | MerkleTreeNode a (MerkleTree a) (MerkleTree a)
                   deriving (Eq, Ord, Show, Read)
 
 
+-- print binary tree in list format
 printMerkleTree EmptyMerkleTree               = "root node"
 printMerkleTree (MerkleTreeNode a left right) = unlines (printMerkleTree_helper (MerkleTreeNode a left right))
 
@@ -18,3 +20,4 @@ printMerkleTree_helper (MerkleTreeNode a left right) = show a : printMerkleTree_
                 ((showTree "+- " "|  ") (printMerkleTree_helper right))
                     ++ (showTree "`- " "   ") (printMerkleTree_helper left)
             showTree first rest = zipWith (++) (first : repeat rest)
+
